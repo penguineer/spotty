01fe8d798d1b8d2b69be1e6c66388d7d29532fb9
Stefan Haun <tux@netz39.de>
2020-10-24 20:22:18 +0200

    Firmware, 2

---
+++ firmware/Spotty/Spotty.ino
@@ -0,0 +1,87 @@
+#include "Wire.h"
+#include <Adafruit_PWMServoDriver.h>
+
+#define LED_WIFI 1
+#define LED_ACT 2
+#define LED_ERR 3
+
+#define PCA9685_I2C_ADDR 64
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+void setLed(int id, float ratio) {
+    int val = floor(ratio*4096.0);
+    pwm.setPWM(id,
+                0,
+               val);
+}
+
+int idxLedIndicator(int channel) {
+    if ((channel < 1) || (channel > 5))
+        return 0;
+
+    static int idx[5] = {14, 12, 10, 4, 6};
+
+    return idx[channel-1];
+}
+
+int idxLedPwm(int channel) {
+    if ((channel < 1) || (channel > 5))
+        return 0;
+
+    static int idx[5] = {13, 11, 9, 5, 7};
+
+    return idx[channel-1];
+}
+
+void setup() {
+  Wire.begin(5, 4); // SDA, SCL
+  Serial.begin(115000);
+
+  pinMode(12, OUTPUT);
+  digitalWrite(12, LOW);
+
+  pwm.setPWMFreq(1600);
+  pwm.begin();
+
+  // switch off all LEDs
+  for (int i = 0; i < 16; i++)
+      setLed(i, false);
+  for (int i = 1; i < 6; i++)
+    setLed(idxLedPwm(i), false);
+
+  Serial.write("\nSetup finished.\n");
+}
+
+
+void loop() {
+    setLed(LED_ACT, true);
+
+    for (int i = 0; i < 6; i++) {
+        setLed(idxLedIndicator(i), 1);
+        setLed(idxLedPwm(i), 0.99);
+        delay(500);
+        setLed(idxLedIndicator(i), 0);
+        setLed(idxLedPwm(i), 0);
+    }
+
+    delay(500);
+
+    for (int i = 0; i < 6; i++) {
+        setLed(idxLedIndicator(i), 1);
+        setLed(idxLedPwm(i), 0.99);
+    }
+
+    delay(5000);
+
+    for (int v = 0; v < 4096; v++) {
+        float r = v/4096.0;
+        for (int i = 1; i < 6; i++) {
+            setLed(idxLedPwm(i), r);
+        }
+    }
+
+    Serial.write("Loop end.\n");
+}
+
+
